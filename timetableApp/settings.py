1072fa8ae09af1f0afe68a8cc026e5c035af71fa
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-04-22 04:00:57 +0100

    Database changed to psql v1

--- timetableApp/settings.py
+++ timetableApp/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'timetableApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'd8nh9h61j96v8n'),
+        'PORT': '5432'
     }
 }
 
