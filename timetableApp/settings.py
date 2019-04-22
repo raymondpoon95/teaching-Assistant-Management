7c68716ac2900e1d5843ad45d49291b714e775b9
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-04-22 04:09:42 +0100

    Database changed v3

--- timetableApp/settings.py
+++ timetableApp/settings.py
@@ -81,13 +81,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd8nh9h61j96v8n',
-        'PORT': '5432'
+        'USER': ''
+        'PORT': '5432',
+        'HOST': 'ec2-23-21-136-232.compute-1.amazonaws.com',
     }
 }
 
-import dj_database_url
+# import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
