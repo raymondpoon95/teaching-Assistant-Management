795330f73dc1543ed207cb6a4706976653fcfbc1
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-04-22 03:10:09 +0100

    Added django-heroku

--- timetableApp/settings.py
+++ timetableApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b*=l=rzt@k#+=yrbx1wuytg0auv)ink5)u&d9myz4!%=+7!ju^'
+# SECRET_KEY = 'b*=l=rzt@k#+=yrbx1wuytg0auv)ink5)u&d9myz4!%=+7!ju^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
