1d4c7e4f3e7ee2ce9bf0e82f3058a51e77772bc9
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-10-31 21:50:32 +0000

    initial commit

--- timetableApp/settings.py
+++ timetableApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tahubapp.herokuapp.com']
+ALLOWED_HOSTS = ['tahubapp.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
@@ -84,9 +84,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+# import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
